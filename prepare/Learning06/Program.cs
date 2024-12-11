dc40f4c2e07ddb1118d547fb1d0b6c1c14b0a072
anwuliagbese <jaagbese@gmail.com>
2024-12-11 21:13:02 +0100

    Learning 06 finished

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+         // Create a list of shapes
+        List<Shape> shapes = new List<Shape>();
+
+        // Add shapes to the list
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 5, 3));
+        shapes.Add(new Circle("Green", 2.5));
+
+        // Iterate through the list and display color and area
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.Color}, Area: {shape.GetArea():F2}");
+        }
     }
 }
